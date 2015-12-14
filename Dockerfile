1a4fc6e194e328aeef2e26ab18b01c370c7b1955
Allan Degnan <allan@adegnan.net>
2015-12-14 15:35:09 +0000

    added dockerfile that'll install keycloak-proxy and an entry point to launch it

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM quay.io/ukhomeofficedigital/java7-mvn
+
+RUN yum install -y unzip && yum clean all
+
+RUN adduser app -d /opt/jboss
+ENV VERSION 1.7.0.Final
+
+WORKDIR /opt/jboss
+RUN curl -o keycloak.zip https://downloads.jboss.org/keycloak/${VERSION}/keycloak-proxy-${VERSION}.zip && unzip keycloak.zip && rm keycloak.zip && mv keycloak-proxy-${VERSION} keycloak-proxy
+
+COPY entry-point.sh /entry-point.sh
+ENTRYPOINT /entry-point.sh
---
+++ entry-point.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Pickup any secrets
+for f in /etc/secrets/* ; do
+    if test -f "$f"; then
+        export $(basename $f)="$(eval "echo \"`<$f`\"")"
+    fi
+done 
+
+cat <<- EOF > ${PWD}/keycloak-proxy/config.json
+{
+  "target-url": "${targeturl}",
+  "send-access-token": ${sendaccesstoken:-true},
+  "http-port": "${httpport}",
+  "applications": [
+    {
+      "base-path": "/",
+      "adapter-config": {
+        "realm": "${realm}",
+        "realm-public-key": "${realmpublickey}",
+        "auth-server-url": "${authserverurl}",
+        "ssl-required": "${sslrequired:-external}",
+        "resource": "${resource}",
+        "credentials": {
+          "secret": "${secret}"
+        }
+      },
+      "constraints": [
+        {
+          "pattern": "${pattern}",
+          "authenticate": true
+        }
+      ]
+    }
+  ]
+}
+EOF
+
+exec java -jar ${PWD}/keycloak-proxy/bin/launcher.jar ${PWD}/keycloak-proxy/config.json
